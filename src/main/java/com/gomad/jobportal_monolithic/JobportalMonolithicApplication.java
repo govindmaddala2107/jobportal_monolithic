7a65d6401a3350c5d3207b558cb53376ef2610b6
govindmaddala2107 <maddala.govind2107@gmail.com>
2025-07-02 20:08:48 +0530

    first commit

---
+++ src/main/java/com/gomad/jobportal_monolithic/JobportalMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.gomad.jobportal_monolithic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportalMonolithicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportalMonolithicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomad/jobportal_monolithic/JobportalMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomad.jobportal_monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalMonolithicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
